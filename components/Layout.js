3e085594af44498a5bc29598e114ef2c3227fb58
jonashenrique90 <jonas.herique90@gmail.com>
2021-01-27 04:58:56 -0300

    Add ArticlesList to App.

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import Nav from './Nav'
+import Meta from './Meta'
+import Header from './Header'
+import styles from '../styles/Layout.module.css'
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Meta></Meta>
+            <Nav></Nav>
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    <Header></Header>
+                    { children }
+                </main>              
+            </div>
+        </>
+    )
+}
+
+export default Layout
